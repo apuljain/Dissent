64bedba40cd36e476d5d7bf0d5a82d9eb4bd544e
apuljain <18apul@gmail.com>
2012-07-10 12:18:08 -0400

    Id initialization updated, separate test function added

--- src/Tests/AuthenticateTest.cpp
+++ src/Tests/AuthenticateTest.cpp
@@ -2,28 +2,33 @@
 
 namespace Dissent {
 namespace Tests {
+
+  void AuthPass(const Id &authe_id, NullAuthenticate *authe, NullAuthenticator *autho)
+  {
+    QVariant m1 = authe->PrepareForChallenge();
+    QVariant m2 = autho->RequestChallenge(authe_id, m1);
+    QPair<bool,QVariant> r1 = authe->ProcessChallenge(m2);
+
+    EXPECT_TRUE(r1.first);
+
+    QPair<bool,PublicIdentity> r2 = autho->VerifyResponse(authe_id, r1.second);
+    EXPECT_TRUE(r2.first);
+    EXPECT_EQ(r2.second, GetPublicIdentity(authe->GetPrivateIdentity()));
+  }
+
   TEST(Authenticate, Null) {
 
     Crypto::Library *lib = Crypto::CryptoFactory::GetInstance().GetLibrary();
-    QByteArray id(20, 0);
-    lib->GetRandomNumberGenerator()->GenerateBlock(id);
 
-    PrivateIdentity client = PrivateIdentity(Id(id),QSharedPointer<AsymmetricKey>(lib->GeneratePrivateKey(id)),
-      QSharedPointer<DiffieHellman>(lib->GenerateDiffieHellman(id)));
+    Id id;
+
+    PrivateIdentity client = PrivateIdentity(id,QSharedPointer<AsymmetricKey>(lib->GeneratePrivateKey(id.GetByteArray())),
+    QSharedPointer<DiffieHellman>(lib->GenerateDiffieHellman(id.GetByteArray())));
 
     NullAuthenticate auth_client(client);
     NullAuthenticator auth_leader;
 
-    QVariant m1 = auth_client.PrepareForChallenge();
-    QVariant m2 = auth_leader.RequestChallenge(client.GetLocalId(), m1);
-    QPair<bool,QVariant> r1 = auth_client.ProcessChallenge(m2);
-
-    EXPECT_TRUE(r1.first);
-
-    QPair<bool,PublicIdentity> r2 = auth_leader.VerifyResponse(client.GetLocalId(), r1.second);
-    EXPECT_TRUE(r2.first);
-    EXPECT_EQ(r2.second, GetPublicIdentity(client));
+    AuthPass(id, &auth_client, &auth_leader);
   }
-
 }
 }
