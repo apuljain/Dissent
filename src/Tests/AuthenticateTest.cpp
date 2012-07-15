c76f30dd2e3d60cfe8bf35d7f8891ae6c2c749e3
apuljain <18apul@gmail.com>
2012-07-14 21:51:20 -0400

    Testing code for LRS authenticat*

--- src/Tests/AuthenticateTest.cpp
+++ src/Tests/AuthenticateTest.cpp
@@ -16,7 +16,8 @@ namespace Tests {
     EXPECT_EQ(r2.second, GetPublicIdentity(authe->GetPrivateIdentity()));
   }
 
-  TEST(Authenticate, Null) {
+  TEST(Authenticate, Null)
+  {
 
     Crypto::Library *lib = Crypto::CryptoFactory::GetInstance().GetLibrary();
 
@@ -29,6 +30,70 @@ namespace Tests {
     NullAuthenticator auth_leader;
 
     AuthPass(id, &auth_client, &auth_leader);
+  }
+
+  TEST(Authenticate, LRS)
+  {
+    QVector<QSharedPointer<PublicIdentity> > public_idents;
+    QVector<QSharedPointer<PrivateIdentity> > priv_idents;
+
+    int num_members = 5;
+
+    QSharedPointer<CppDsaPrivateKey> base_key(new CppDsaPrivateKey());
+    Integer generator = base_key->GetGenerator();
+    Integer subgroup = base_key->GetSubgroup();
+    Integer modulus = base_key->GetModulus();
+
+    for(int i = 0; i < num_members; i++)
+    {
+      QSharedPointer<CppDsaPrivateKey> private_key(new CppDsaPrivateKey(modulus, subgroup, generator));
+
+      Id id(i);
+
+      QSharedPointer<PrivateIdentity> pr_id = QSharedPointer<PrivateIdentity>
+        (new PrivateIdentity(id, QSharedPointer<AsymmetricKey>(private_key), QSharedPointer<DiffieHellman>(), true));
+
+      priv_idents.append(pr_id);
+
+      AsymmetricKey *key = 0;
+      QByteArray dh_pub = QByteArray();
+
+      if(pr_id->GetSigningKey())
+      {
+        key = pr_id->GetSigningKey()->GetPublicKey();
+      }
+
+      if(pr_id->GetDhKey())
+      {
+        dh_pub = pr_id->GetDhKey()->GetPublicComponent();
+      }
+
+      QSharedPointer<AsymmetricKey> skey(key);
+
+      QSharedPointer<PublicIdentity> pub_id = QSharedPointer<PublicIdentity>
+        (new PublicIdentity(pr_id->GetLocalId(), skey, dh_pub, pr_id->GetSuperPeer()));
+
+      public_idents.append(pub_id);
+    }
+
+    //Testing private_public key pairs.
+    for(int j = 0; j < num_members; j++)
+    {
+      QSharedPointer<CppDsaPublicKey> t1 = public_idents[j]->GetVerificationKey().dynamicCast<CppDsaPublicKey>();
+      QSharedPointer<CppDsaPrivateKey> t2 = priv_idents[j]->GetSigningKey().dynamicCast<CppDsaPrivateKey>();
+      EXPECT_EQ(generator.Pow(t2->GetPrivateExponent(), modulus), t1->GetPublicElement());
+      EXPECT_EQ(priv_idents[j]->GetLocalId(), public_idents[j]->GetId());
+    }
+
+    //Instantiate client and server objects.
+    LRSAuthenticate auth_client(public_idents, priv_idents[0], generator, modulus, subgroup, 0);
+    LRSAuthenticator auth_leader(public_idents, generator, modulus, subgroup);
+
+    QVariant m1 = auth_client.PrepareForChallenge();
+
+    QPair<bool, PublicIdentity> r2 = auth_leader.VerifyResponse(priv_idents[0]->GetLocalId(), m1);
+
+    EXPECT_TRUE(r2.first);
   }
 }
 }
