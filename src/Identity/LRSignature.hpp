4082233057f630a0d5b0f468827e5054d517c715
apuljain <18apul@gmail.com>
2012-07-14 21:49:12 -0400

    Indentation updated and minor changes to class variables.

--- src/Identity/LRSignature.hpp
+++ src/Identity/LRSignature.hpp
@@ -1,46 +1,59 @@
 #ifndef DISSENT_IDENTITY_LRS_SIGNATURE_H_GUARD
 #define DISSENT_IDENTITY_LRS_SIGNATURE_H_GUARD
 
+#include <QMetaType>
 #include "Crypto/Integer.hpp"
 
 namespace Dissent {
 namespace Identity {
-
-  class LRSignature {
-
-    private:
-
-      const Integer _c, _linkage_tag;
-      const QVector<Integer> _s;
+  /**
+   * Signature class. It holds LR Signature components.
+   */
+  class LRSignature : public QObject {
+    Q_OBJECT
 
     public:
-
       typedef Crypto::Integer Integer;
+
+      virtual ~LRSignature() {}
+
+      LRSignature() {}
 
       /**
        * Constructor to initialize signature object.
        * @param: signature components.
        */
-      explicit LRSignature(const Integer &c, const QVector<Integer> &s,
-                           const Integer &tag):
+      explicit LRSignature(const Integer &c, const QVector<Integer> &s, const Integer &tag):
       _c(c), _linkage_tag(tag), _s(s)
       {
       }
 
-      inline const Integer &GetC() {return _c;}
+      inline Integer GetC() {return _c;}
 
-      inline const QVector<Integer> &GetS() {return _s;}
+      inline QVector<Integer> GetS() {return _s;}
 
-      inline const Integer &GetSi(const int &index) {return _s[index];}
+      inline Integer& GetSi(const int &index) {return _s[index];}
 
-      inline const Integer &GetTag() {return _linkage_tag;}
+      inline Integer& GetTag() {return _linkage_tag;}
 
       void SetTag(const Integer &tag) {_linkage_tag = tag;}
 
       void SetC(const Integer &c) {_c = c;}
 
-      void SetS(const QVector<Integer> &s) {_s = s;}
+      void SetS(QVector<Integer> &s)
+      {
+        _s.clear();
+        for(QVector<Integer>::const_iterator itr = s.begin(); itr != s.end(); itr++)
+        {
+          _s.push_back(*itr);
+        }
+      }
+
+    private:
+      Integer _c, _linkage_tag;
+      QVector<Integer> _s;
    };
+ }
 }
-}
+Q_DECLARE_METATYPE(QSharedPointer<Dissent::Identity::LRSignature>)
 #endif
