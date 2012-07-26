f8822fa74414b1630d9470e1ffc058099344c51d
Apul <18apul@gmail.com>
2012-07-27 05:10:44 +0530

    Code loads DSA keys from files generated by keygen utility in private and public directories.

--- src/Tests/AuthenticateTest.cpp
+++ src/Tests/AuthenticateTest.cpp
@@ -48,21 +48,41 @@ namespace Tests {
    * @param group parameters - generator, modulus and subgroup size.
    */
   void LRSAuthPass(QVector<QSharedPointer<PublicIdentity> > &public_idents,
-    QVector<QSharedPointer<PrivateIdentity> > &priv_idents,
-    Integer &generator, Integer &modulus, Integer &subgroup)
+    QVector<QSharedPointer<PrivateIdentity> > &priv_idents)
   {
     //Instantiate client and server objects.
 
-    LRSAuthenticate auth_client(public_idents, priv_idents[0], generator,
-                                 modulus, subgroup);
-    LRSAuthenticator auth_leader(public_idents, generator, modulus, subgroup);
+    QSharedPointer<Crypto::CppDsaPrivateKey> priv_key =
+      priv_idents[0]->GetSigningKey().dynamicCast<CppDsaPrivateKey>();
+
+    priv_key->GetModulus();
+
+    LRSAuthenticate auth_client(public_idents, priv_idents[0]);
+    LRSAuthenticator auth_leader(public_idents);
+
+    QFile file("/home/apul/intern/repo/Dissent_david/BenchMark.txt");
+    file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append);
+    QTextStream out(&file);
+
+    qint64 time_start = QDateTime::currentMSecsSinceEpoch();
 
     QVariant m1 = auth_client.PrepareForChallenge();
+    qint64 time_taken1 = QDateTime::currentMSecsSinceEpoch() - time_start;
+
+    time_start = QDateTime::currentMSecsSinceEpoch();
+
+    out << "LRS Generation time for " << public_idents.count() << " members is: " << time_taken1 << " ms.\n";
+
     QPair<bool, PublicIdentity> r2 = auth_leader.VerifyResponse(
                                        priv_idents[0]->GetLocalId(), m1);
 
+    qint64 time_taken2 = QDateTime::currentMSecsSinceEpoch() - time_start;
+    out << "LRS Verification time for " << public_idents.count() << " members is: " << time_taken2 << " ms.\n";
+    out << "LRS Total time for: " << public_idents.count() << " members is: " << (time_taken1 + time_taken2) << " ms.\n";
+    file.close();
+
     EXPECT_TRUE(r2.first);
-  }
+ }
 
   /**
    * Function to test LRS code for fail test cases.
@@ -71,22 +91,23 @@ namespace Tests {
    * @param group parameters - generator, modulus and subgroup size.
    */
   void LRSAuthFail(QVector<QSharedPointer<PublicIdentity> > &public_idents,
-    QVector<QSharedPointer<PrivateIdentity> > &priv_idents,
-    Integer &generator, Integer &modulus, Integer &subgroup)
+    QVector<QSharedPointer<PrivateIdentity> > &priv_idents)
   {
     //Fail due to invalid public_private key pair for authenticating client.
     //Instantiate client and server objects.
 
-//    QSharedPointer<CppDsaPrivateKey> private_key(new CppDsaPrivateKey(modulus,
-//                                                    subgroup, generator));
+//    QSharedPointer<CppDsaPrivateKey> t2 =
+//      priv_idents[0]->GetSigningKey().dynamicCast<CppDsaPrivateKey>();
+
+//    QSharedPointer<CppDsaPrivateKey> private_key(new CppDsaPrivateKey(t2->GetModulus(), t2->GetSubgroup(), t2->GetGenerator()));
 //    Id id(1);
 //    QSharedPointer<PrivateIdentity> pr_id = QSharedPointer<PrivateIdentity>
 //     (new PrivateIdentity(id, QSharedPointer<AsymmetricKey>(private_key),
 //                           QSharedPointer<DiffieHellman>(), true));
 
-//    LRSAuthenticate auth_client_test1(public_idents, pr_id, generator,
-//                                       modulus, subgroup);
-//    LRSAuthenticator auth_leader_test1(public_idents, generator, modulus, subgroup);
+//    LRSAuthenticate auth_client_test1(public_idents, pr_id);
+
+//    LRSAuthenticator auth_leader_test1(public_idents);
 
 //    QVariant m1 = auth_client_test1.PrepareForChallenge();
 //    QPair<bool, PublicIdentity> r2 = auth_leader_test1.VerifyResponse(
@@ -95,80 +116,93 @@ namespace Tests {
 //    EXPECT_FALSE(r2.first);
 
     //Fail due to different sets of public keys with client and leader.
-    LRSAuthenticate auth_client_test3(public_idents, priv_idents[0], generator,
-                                       modulus, subgroup);
+    LRSAuthenticate auth_client_test3(public_idents, priv_idents[0]);
     public_idents[0] = public_idents[1];
-    LRSAuthenticator auth_leader_test3(public_idents, generator, modulus,
-                                        subgroup);
+    LRSAuthenticator auth_leader_test3(public_idents);
     QVariant m1 = auth_client_test3.PrepareForChallenge();
     QPair<bool, PublicIdentity> r2 = auth_leader_test3.VerifyResponse(priv_idents[0]->GetLocalId(), m1);
 
     EXPECT_FALSE(r2.first);
-}
+  }
 
   TEST(Authenticate, LRS)
   {
     QVector<QSharedPointer<PublicIdentity> > public_idents;
     QVector<QSharedPointer<PrivateIdentity> > priv_idents;
 
-    int num_members = 5;
+    QDir pubdir("public");
+    QDir privdir("private");
 
-    QSharedPointer<CppDsaPrivateKey> base_key(new CppDsaPrivateKey());
-    Integer generator = base_key->GetGenerator();
-    Integer subgroup = base_key->GetSubgroup();
-    Integer modulus = base_key->GetModulus();
+    pubdir.setFilter(QDir::NoDotAndDotDot | QDir::Files | QDir::NoSymLinks);
+    privdir.setFilter(QDir::NoDotAndDotDot | QDir::Files | QDir::NoSymLinks);
 
-    for(int i = 0; i < num_members; i++)
+    QStringList list_pub = pubdir.entryList();
+    QStringList list_priv = privdir.entryList();
+
+    int num_members = list_pub.count();
+
+    for(int j = 2; j < num_members + 1; j = j*2)
     {
-      QSharedPointer<CppDsaPrivateKey> private_key(new CppDsaPrivateKey(modulus,
-                                                    subgroup, generator));
+        public_idents.clear();
+        priv_idents.clear();
+        for(int i = 0; i < j; i++)
+        {
+            QSharedPointer<CppDsaPrivateKey> private_key(
+              new CppDsaPrivateKey("private/" + list_priv.at(i)));
 
-      Id id(i);
-      QSharedPointer<PrivateIdentity> pr_id = QSharedPointer<PrivateIdentity>
-        (new PrivateIdentity(id, QSharedPointer<AsymmetricKey>(private_key),
-                             QSharedPointer<DiffieHellman>(), true));
+            Id id(i);
+            QSharedPointer<PrivateIdentity> pr_id = QSharedPointer<PrivateIdentity>
+              (new PrivateIdentity(id, QSharedPointer<AsymmetricKey>(private_key),
+                                 QSharedPointer<DiffieHellman>(), true));
 
-      priv_idents.append(pr_id);
+            priv_idents.append(pr_id);
 
-      AsymmetricKey *key = 0;
-      QByteArray dh_pub = QByteArray();
+            AsymmetricKey *key = 0;
+            QByteArray dh_pub = QByteArray();
 
-      if(pr_id->GetSigningKey())
-      {
-        key = pr_id->GetSigningKey()->GetPublicKey();
-      }
+            if(pr_id->GetSigningKey())
+            {
+              key = pr_id->GetSigningKey()->GetPublicKey();
+            }
 
-      if(pr_id->GetDhKey())
-      {
-        dh_pub = pr_id->GetDhKey()->GetPublicComponent();
-      }
+            if(pr_id->GetDhKey())
+            {
+              dh_pub = pr_id->GetDhKey()->GetPublicComponent();
+            }
 
-      QSharedPointer<AsymmetricKey> skey(key);
+            QSharedPointer<AsymmetricKey> skey(key);
 
-      QSharedPointer<PublicIdentity> pub_id = QSharedPointer<PublicIdentity>
-        (new PublicIdentity(pr_id->GetLocalId(), skey, dh_pub,
-                             pr_id->GetSuperPeer()));
+            QSharedPointer<PublicIdentity> pub_id = QSharedPointer<PublicIdentity>
+              (new PublicIdentity(pr_id->GetLocalId(), skey, dh_pub,
+                                 pr_id->GetSuperPeer()));
 
-      public_idents.append(pub_id);
+            public_idents.append(pub_id);
+         }
+
+        //Call for success test.
+        LRSAuthPass(public_idents, priv_idents);
     }
 
-    //Testing private_public key pairs.
-    for(int j = 0; j < num_members; j++)
-    {
-      QSharedPointer<CppDsaPublicKey> t1 =
-        public_idents[j]->GetVerificationKey().dynamicCast<CppDsaPublicKey>();
-      QSharedPointer<CppDsaPrivateKey> t2 =
-        priv_idents[j]->GetSigningKey().dynamicCast<CppDsaPrivateKey>();
+//    //Testing private_public key pairs.
+//    for(int j = 0; j < num_members; j++)
+//    {
+//      QSharedPointer<CppDsaPublicKey> t1 =
+//        public_idents[j]->GetVerificationKey().dynamicCast<CppDsaPublicKey>();
+//      QSharedPointer<CppDsaPrivateKey> t2 =
+//        priv_idents[j]->GetSigningKey().dynamicCast<CppDsaPrivateKey>();
 
-      EXPECT_EQ(generator.Pow(t2->GetPrivateExponent(), modulus),
-                              t1->GetPublicElement());
-      EXPECT_EQ(priv_idents[j]->GetLocalId(), public_idents[j]->GetId());
-    }
-    //Call for success test.
-    LRSAuthPass(public_idents, priv_idents, generator, modulus, subgroup);
+//      EXPECT_EQ(t1->GetGenerator(), t2->GetGenerator());
+//      EXPECT_EQ(t1->GetModulus(), t2->GetModulus());
+//      EXPECT_EQ(t1->GetSubgroup(), t2->GetSubgroup());
+
+//      EXPECT_EQ(t2->GetGenerator().Pow(t2->GetPrivateExponent(), t2->GetModulus()),
+//                              t1->GetPublicElement());
+//    }
+
 
     //Call for fail tests.
-    LRSAuthFail(public_idents, priv_idents, generator, modulus, subgroup);
+    LRSAuthFail(public_idents, priv_idents);
+
   }
 }
 }
